7c9cf0268d2e511d5ea2c535227e57996ddbc0a8
s-yamasaki0213 <3651999@gmail.com>
2025-02-04 00:05:32 +0900

    kadai_007
    連想配列

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCUTYPE html>
+
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+  </head>
+
+  <body>
+    <?php
+    $item = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($item);
+    ?>
+  </body>
+</html>
